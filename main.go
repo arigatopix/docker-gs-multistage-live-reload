f0d978dd7ab75e5b909df5722897db063fd51c95
arigato <teeruch.k@gmail.com>
2022-06-29 20:37:41 +0700

    live reload - w air

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ func main() {
 		return c.HTML(http.StatusOK, "Hello, Docker! <3")
 	})
 
+	e.GET("/hello", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "use air live reload")
+	})
+
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
 	})
